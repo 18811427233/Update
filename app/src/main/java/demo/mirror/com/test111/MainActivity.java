4bbaddac4d82307f47d1f557eae23912f6437e43
ZHANG ZHAUNG <279866275@qq.com>
2017-10-30 16:41:49 +0800

    7

--- app/src/main/java/demo/mirror/com/test111/MainActivity.java
+++ app/src/main/java/demo/mirror/com/test111/MainActivity.java
@@ -13,9 +13,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String url = "http://101.28.249.94/apk.r1.market.hiapk.com/data/upload/apkres/2017/4_11/15/com.baidu.searchbox_034250.apk";
+        /*该值为：AndroidManifest内的provider，name值，务必填写一致*/
+        String providerFile = "demo.mirror.com.myupdate.fileprovider";
         /*调用更新方法*/
         UpdateManager.update(this
-                , "demo.mirror.com.myupdate.fileprovider"
+                , providerFile
                 , url);
 
     }
