3be5cf944ed928651336bbca56a60b232307f317
ZHANG ZHAUNG <279866275@qq.com>
2017-10-30 16:35:09 +0800

    7

--- app/src/main/java/demo/mirror/com/test111/MainActivity.java
+++ app/src/main/java/demo/mirror/com/test111/MainActivity.java
@@ -1,7 +1,9 @@
 package demo.mirror.com.test111;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import demo.mirror.com.myupdate.UpdateManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "http://101.28.249.94/apk.r1.market.hiapk.com/data/upload/apkres/2017/4_11/15/com.baidu.searchbox_034250.apk";
+        /*调用更新方法*/
+        UpdateManager.update(this
+                , "demo.mirror.com.myupdate.fileprovider"
+                , url);
+
     }
 }
